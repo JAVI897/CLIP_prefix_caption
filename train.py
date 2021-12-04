89c648f66b5469cb6dbe4d1b845470118e2fde94
Javi GG <s.javi.com@gmail.com>
2021-12-04 12:27:46 +0100

    Fix save_config error

--- train.py
+++ train.py
@@ -261,7 +261,9 @@ class ClipCaptionPrefix(ClipCaptionModel):
 def save_config(args: argparse.Namespace):
     config = {}
     for key, item in args._get_kwargs():
-        config[key] = item
+        if key != 'mapping_type':
+            print('Savinf config. Key: {} = {}'.format(key, item))
+            config[key] = item
     out_path = os.path.join(args.out_dir, f"{args.prefix}.json")
     with open(out_path, 'w') as outfile:
         json.dump(config, outfile)
