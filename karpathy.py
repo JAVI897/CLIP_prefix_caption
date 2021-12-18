6f484c67e288adfd9456dbdc918a6eb2e62cb148
Javi GG <s.javi.com@gmail.com>
2021-12-18 08:51:37 +0100

    Update karpathy.py

--- karpathy.py
+++ karpathy.py
@@ -8,7 +8,6 @@ import sys
 from typing import Tuple, List, Union, Optional
 from transformers import GPT2Tokenizer, GPT2LMHeadModel, AdamW, get_linear_schedule_with_warmup
 from tqdm import tqdm, trange
-from google.colab import files
 import skimage.io as io
 import PIL.Image
 from IPython.display import Image 
