94ac4c6199574e22378e0ba6937346f4386f68bd
Javi GG <s.javi.com@gmail.com>
2022-01-02 19:33:29 +0100

    Update problem.py

--- problem.py
+++ problem.py
@@ -1,6 +1,6 @@
 import numpy as np
 import torch
-from pymoo.model.problem import Problem
+from pymoo.core.problem import Problem
 from utils import generate_beam, compute_metrics, best_n_sim_clip
 
 class ClipGAProblem(Problem):
@@ -10,7 +10,8 @@ class ClipGAProblem(Problem):
 		self.config = config
 
 	def _evaluate(self, x, out, *args, **kwargs):
-		text_captions = generate_beam(self.config.model, self.config.tokenizer, beam_size=self.config.beam_size, embed=x)
+		x_reshaped = x.reshape(1, self.config.prefix_length, -1)
+		text_captions = generate_beam(self.config.model, self.config.tokenizer, beam_size=self.config.beam_size, embed=x_reshaped)
 		text_caption, clip_sim, hypothesis = best_n_sim_clip(text_captions, self.config.prefix, self.config.clip_model, self.config.device, 
 															 similarity = self.config.similarity_clip)
 		out["F"] = -clip_sim
