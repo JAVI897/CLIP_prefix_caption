c27de7534228fb026287a3203842cbf6a2263c1c
Javi GG <s.javi.com@gmail.com>
2022-01-02 20:34:57 +0100

    Update problem.py

--- problem.py
+++ problem.py
@@ -4,12 +4,12 @@ from pymoo.core.problem import ElementwiseProblem
 from utils import generate_beam, compute_metrics, best_n_sim_clip
 from pymoo.core.problem import Problem
 
-class ClipGAProblem(Problem):
+class ClipGAProblem(ElementwiseProblem):
 
 	def __init__(self, config, **kwargs):
 		xl = -10*np.ones(768*40)
 		xu =  10*np.ones(768*40)
-		super().__init__(n_var=768*40, n_obj=1, n_constr=0, xl=xl, xu=xu, elementwise_evaluation=True, **kwargs)
+		super().__init__(n_var=768*40, n_obj=1, n_constr=0, xl=xl, xu=xu, **kwargs)
 		self.config = config
 
 	def _evaluate(self, x, out, *args, **kwargs):
