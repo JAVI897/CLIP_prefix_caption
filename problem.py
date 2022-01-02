acd2c1b0a83a366afbe4a19342fb49156c69d356
Javi GG <s.javi.com@gmail.com>
2022-01-02 20:28:57 +0100

    Update problem.py

--- problem.py
+++ problem.py
@@ -2,13 +2,14 @@ import numpy as np
 import torch
 from pymoo.core.problem import ElementwiseProblem
 from utils import generate_beam, compute_metrics, best_n_sim_clip
+from pymoo.core.problem import Problem
 
-class ClipGAProblem(ElementwiseProblem):
+class ClipGAProblem(Problem):
 
 	def __init__(self, config):
 		xl = -10*np.ones(768*40)
 		xu =  10*np.ones(768*40)
-		super().__init__(n_var=768*40, n_obj=1, n_constr=1, xl=xl, xu=xu)
+		super().__init__(n_var=768*40, n_obj=1, n_constr=1, xl=xl, xu=xu, elementwise_evaluation=True)
 		self.config = config
 
 	def _evaluate(self, x, out, *args, **kwargs):
