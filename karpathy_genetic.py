99f8ec2b8ef129a3853ae3cc511cfc5dfb02c449
Javi GG <s.javi.com@gmail.com>
2022-01-02 19:42:10 +0100

    Update karpathy_genetic.py

--- karpathy_genetic.py
+++ karpathy_genetic.py
@@ -35,8 +35,9 @@ def configuration():
 def genetic_alg(prefix_embed, config):
 	population_size = 100
 	# Generate initial population
-	initial_solutions = [ prefix_embed + torch.randn(prefix_embed.shape[0]).to(config['device'], dtype=torch.float32) for i in range(population_size)]
-	initial_solutions = torch.cat(initial_solutions, 1)
+	initial_solutions = [ prefix_embed + torch.randn(prefix_embed.shape[0]).to(config['device'], dtype=torch.float32) for i in range(population_size - 1)]
+	initial_solutions + [prefix_embed]
+	initial_solutions = torch.cat(initial_solutions, 0)
 	print(initial_solutions)
 	print(initial_solutions.shape)
 	"""
