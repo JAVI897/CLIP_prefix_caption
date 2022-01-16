d3e5e6c10b5892eb100931478b3cf32688648569
Javi GG <s.javi.com@gmail.com>
2022-01-16 12:42:40 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -130,7 +130,6 @@ def generate_based_on_clipscore(
 
 				indices_to_remove = sorted_indices[sorted_indices_to_remove]
 				logits[:, indices_to_remove] = filter_value
-				logits = logits.softmax(-1).log()
 				if tokens is not None:
 					Z = torch.zeros(*logits.shape).to(device)
 					for j in range(10):
@@ -147,7 +146,7 @@ def generate_based_on_clipscore(
 						#print(Z[ :, aux_next_token] + logits[ :, aux_next_token])
 						print(aux_text, 'CLIPScore: ', clipscore)
 					print('----------------')
-					logits = logits + Z
+					logits = Z
 				next_token = torch.argmax(logits, -1).unsqueeze(0)
 				next_token_embed = model.gpt.transformer.wte(next_token)
 				if tokens is None:
