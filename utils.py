7577e3e2ee98687057e7285b06339fdad9d4c5fd
Javi GG <s.javi.com@gmail.com>
2022-01-16 13:05:29 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -92,70 +92,54 @@ def generate_based_on_clipscore(
 		tokens=None,
 		prompt=None,
 		embed=None,
-		entry_count=1,
 		entry_length=67,  # maximum number of words
-		top_p=0.8,
 		temperature=1.,
 		stop_token: str = '.',
 ):
 	model.eval()
-	generated_num = 0
-	generated_list = []
 	stop_token_index = tokenizer.encode(stop_token)[0]
-	filter_value = -float("Inf")
 	device = next(model.parameters()).device
-
+	generated = embed
 	with torch.no_grad():
 
-		for entry_idx in trange(entry_count):
-			if embed is not None:
-				generated = embed
+		for i in range(entry_length):
+
+			outputs = model.gpt(inputs_embeds=generated)
+			logits = outputs.logits
+			logits = logits[:, -1, :] / (temperature if temperature > 0 else 1.0)
+			logits = logits.softmax(-1)
+			sorted_logits, sorted_indices = torch.sort(logits, descending=True)
+			if tokens is not None:
+				Z = torch.zeros(*logits.shape).to(device)
+				for j in range(10):
+					aux_next_token = sorted_indices[:,j].unsqueeze(0)
+					aux = torch.cat((tokens, aux_next_token), dim = 1)
+					aux_list = list(aux.squeeze().cpu().numpy())
+					aux_text = tokenizer.decode(aux_list)
+
+					# compute clipscore
+					tokens_clip = clip.tokenize([aux_text]).to(device).long()
+					clip_text = clip_model.encode_text(tokens_clip).detach()
+					clipscore = 2.5*np.clip( torch.cosine_similarity(clip_text, clip_image).cpu().numpy()[0], 0, None)
+					Z[ :, aux_next_token] = clipscore
+					print(Z[ :, aux_next_token] + logits[ :, aux_next_token])
+					print(aux_text, 'CLIPScore: ', clipscore)
+				print('----------------')
+				logits = beta*logits + gamma*Z
+			next_token = torch.argmax(logits, -1).unsqueeze(0)
+			next_token_embed = model.gpt.transformer.wte(next_token)
+			if tokens is None:
+				tokens = next_token
 			else:
-				if tokens is None:
-					tokens = torch.tensor(tokenizer.encode(prompt))
-					tokens = tokens.unsqueeze(0).to(device)
+				tokens = torch.cat((tokens, next_token), dim=1)
+			generated = torch.cat((generated, next_token_embed), dim=1)
+			if stop_token_index == next_token.item():
+				break
 
-				generated = model.gpt.transformer.wte(tokens)
+		output_list = list(tokens.squeeze().cpu().numpy())
+		output_text = tokenizer.decode(output_list)
 
-			for i in range(entry_length):
-
-				outputs = model.gpt(inputs_embeds=generated)
-				logits = outputs.logits
-				logits = logits[:, -1, :] / (temperature if temperature > 0 else 1.0)
-				logits = logits.softmax(-1)
-				sorted_logits, sorted_indices = torch.sort(logits, descending=True)
-				if tokens is not None:
-					Z = torch.zeros(*logits.shape).to(device)
-					for j in range(10):
-						aux_next_token = sorted_indices[:,j].unsqueeze(0)
-						aux = torch.cat((tokens, aux_next_token), dim = 1)
-						aux_list = list(aux.squeeze().cpu().numpy())
-						aux_text = tokenizer.decode(aux_list)
-
-						# compute clipscore
-						tokens_clip = clip.tokenize([aux_text]).to(device).long()
-						clip_text = clip_model.encode_text(tokens_clip).detach()
-						clipscore = 2.5*np.clip( torch.cosine_similarity(clip_text, clip_image).cpu().numpy()[0], 0, None)
-						Z[ :, aux_next_token] = clipscore
-						print(Z[ :, aux_next_token] + logits[ :, aux_next_token])
-						print(aux_text, 'CLIPScore: ', clipscore)
-					print('----------------')
-					logits = beta*logits + gamma*Z
-				next_token = torch.argmax(logits, -1).unsqueeze(0)
-				next_token_embed = model.gpt.transformer.wte(next_token)
-				if tokens is None:
-					tokens = next_token
-				else:
-					tokens = torch.cat((tokens, next_token), dim=1)
-				generated = torch.cat((generated, next_token_embed), dim=1)
-				if stop_token_index == next_token.item():
-					break
-
-			output_list = list(tokens.squeeze().cpu().numpy())
-			output_text = tokenizer.decode(output_list)
-			generated_list.append(output_text)
-
-	return generated_list[0]
+	return output_text
 
 def generate2(
 		model,
