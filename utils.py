8f58b098c9d8bfdbb512e6c2aae9f28a46c3db77
Javi GG <s.javi.com@gmail.com>
2022-01-16 13:18:38 +0100

    debug...

--- utils.py
+++ utils.py
@@ -122,9 +122,9 @@ def generate_based_on_clipscore(
 					clip_text = clip_model.encode_text(tokens_clip).detach()
 					clipscore = 2.5*np.clip( torch.cosine_similarity(clip_text, clip_image).cpu().numpy()[0], 0, None)
 					Z[ :, aux_next_token] = clipscore
-					print(Z[ :, aux_next_token] + logits[ :, aux_next_token])
-					print(aux_text, 'CLIPScore: ', clipscore)
-				print('----------------')
+					#print(beta*Z[ :, aux_next_token] + gamma*logits[ :, aux_next_token])
+					#print(aux_text, 'CLIPScore: ', clipscore)
+				#print('----------------')
 				logits = beta*logits + gamma*Z
 			next_token = torch.argmax(logits, -1).unsqueeze(0)
 			next_token_embed = model.gpt.transformer.wte(next_token)
@@ -142,7 +142,7 @@ def generate_based_on_clipscore(
 		tokens_clip = clip.tokenize([output_text]).to(device).long()
 		clip_text = clip_model.encode_text(tokens_clip).detach()
 		clipscore_text = 2.5*np.clip( torch.cosine_similarity(clip_text, clip_image).cpu().numpy()[0], 0, None)
-		
+
 	return output_text, clipscore_text
 
 def generate2(
